153b1a7deaf6b447fb23d953b2080356c63e4481
Dahyuns <77180432+Dahyuns@users.noreply.github.com>
2024-02-02 17:34:46 +0900

    20240202

--- Assets/Scripts/MapTile.cs
+++ Assets/Scripts/MapTile.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Slither
@@ -8,6 +6,7 @@ namespace Slither
     {
         //맵타일 프리팹
         public GameObject mapTilePrefab;
+        private float mapTileSize;
 
         // 맵 9개 정사각형 모양으로 만든후,
         // 가운데 타일을 기준으로 삼고.(계속 바뀜)
@@ -15,6 +14,8 @@ namespace Slither
         // 뒤에는 삭제
         // (( 4개로 해서 좌표받는 방법도 있을듯??
         void Start()
-        { }
+        {
+        
+        }
     }
 }
