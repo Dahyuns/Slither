807e5efe78814bad2ef68bae194956cec63768e0
Dahyuns <77180432+Dahyuns@users.noreply.github.com>
2024-02-23 17:41:31 +0900

    Fix tail size adjustable by scale, mesh size
    메쉬사이즈와 스케일에 따른 꼬리크기 자동변경 스크립트내 버그 고침

--- Assets/Scripts/Player/TailControl.cs
+++ Assets/Scripts/Player/TailControl.cs
@@ -5,9 +5,10 @@ namespace WiggleQuest
 {
     public class TailControl : MonoBehaviour
     {
+        //꼬리 랜덤 생성, 따라서 움직이게 만들기
         private enum Count
         { Up, Down, None }
-        public Transform wormFace;    //지렁이 얼굴
+        public GameObject wormFace;    //지렁이 얼굴
         public GameObject tailPrefab; //꼬리 프리팹
 
         private Tail headTail = null;  //연결리스트 Head
@@ -26,7 +27,7 @@ namespace WiggleQuest
             tailCount = tailStartCount;
 
             //사이즈 구해서 그 뒤에 생성 : 사이즈에 scale곱함
-            tailSize = Vector3.Scale(tailPrefab.GetComponent<MeshRenderer>().bounds.size, tailPrefab.transform.localScale);
+            tailSize = Vector3.Scale(tailPrefab.GetComponent<MeshFilter>().sharedMesh.bounds.size, tailPrefab.transform.localScale);
         }
 
         //count체크하고 메서드호출
@@ -68,7 +69,7 @@ namespace WiggleQuest
         {
             //[2]노드활용!- 연결리스트
             //이번 꼬리 생성! : 얼굴 자식으로
-            Tail newtail = Instantiate(tailPrefab, wormFace).GetComponent<Tail>();
+            Tail newtail = Instantiate(tailPrefab).GetComponent<Tail>();
 
             //만든 꼬리가 하나도 없다면(처음 만드는 꼬리) : 맨처음만 호출
             if (tailMakeCount == 0)
@@ -76,13 +77,12 @@ namespace WiggleQuest
                 //얼굴이 전의 꼬리임
                 newtail.beforeTail = wormFace.GetComponent<Tail>();
                 //이번 꼬리의 위치는? : 얼굴 위치에 + 꼬리 사이즈 (랜덤위치 / 반대쪽 예정)
-                Vector3 faceSize = Vector3.Scale(wormFace.GetComponent<MeshRenderer>().bounds.size,
-                                                                     wormFace.transform.localScale);//이거 잘못된듯!!?
+                Vector3 faceSize = Vector3.Scale(wormFace.GetComponent<MeshFilter>().sharedMesh.bounds.size,
+                                                                     wormFace.transform.localScale);
 
-                newtail.transform.position = new Vector3
-                    (wormFace.transform.position.x + (faceSize.x / 2 + tailSize.x / 2) , //x
-                     0f, 0f);
-                     //wormFace.transform.position.y + ((faceSize.y + tailSize.y) / 2));
+                Vector3 temporaryDivide = (faceSize / 2 + tailSize / 2);
+                newtail.transform.position = wormFace.transform.position + new Vector3(temporaryDivide.x, 0f, temporaryDivide.y);
+                //wormFace.transform.position.y + ((faceSize.y + tailSize.y) / 2));
             }
             //만든 꼬리가 있다면 == beforeTail이 존재한다면
             else if (tailMakeCount > 0)
